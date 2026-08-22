fd66c604d27ed3587f251a3c595edd43cf0d8dd4
Md Parvej <mdparvejme24@gmail.com>
2026-08-23 03:47:33 +0600

    style: refine design tokens and theme provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,17 +4,20 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { Toaster } from "sonner"
 
 import { ChatProvider } from "@/components/chats/chat-provider"
+import { SettingsProvider } from "@/components/settings/settings-provider"
 import { ThemeSwitch } from "@/components/theme-switch"
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
     >
-      <ChatProvider>{children}</ChatProvider>
+      <SettingsProvider>
+        <ChatProvider>{children}</ChatProvider>
+      </SettingsProvider>
       <ThemeSwitch className="fixed right-5 bottom-5 z-50" />
       <Toaster
         theme="system"
